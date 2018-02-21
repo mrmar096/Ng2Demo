0e0c1f53f3fac7f1bff1dcb077d770d5824ef923
mariomunoz <mario.munyoz@innocv.com>
2018-02-21 13:32:21 +0100

    Added product module and component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppComponent } from './app.component';
 import {HeaderComponent} from '../shared/components/header/header.component';
 import {ListComponent, ProductPipe} from '../shared/components/list/list.component';
 import {ProductsService} from '../shared/services/products.service';
+import {ProductModule} from './product/product.module';
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     ListComponent,
-    ProductPipe
+    ProductPipe,
+    ProductModule
   ],
   imports: [
     BrowserModule,
